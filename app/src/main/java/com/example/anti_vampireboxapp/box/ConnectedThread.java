91adb6836b15dd59b29864afbf2f858e47ea4b1d
Luc-Reinders <98775395+Luc-Reinders@users.noreply.github.com>
2022-10-04 14:47:10 +0200

    Added this new branch for the AntiVampBox object development and added two classes that will be used for this development.

---
+++ app/src/main/java/com/example/anti_vampireboxapp/box/ConnectedThread.java
@@ -0,0 +1,5 @@
+package com.example.anti_vampireboxapp.box;
+
+public class ConnectedThread extends Thread {
+
+}
---
+++ app/src/main/java/com/example/anti_vampireboxapp/box/CreateConnectThread.java
@@ -0,0 +1,7 @@
+package com.example.anti_vampireboxapp.box;
+
+public class CreateConnectThread extends Thread{
+
+    //TODO: JOINK CODE FROM INTERNET
+
+}
